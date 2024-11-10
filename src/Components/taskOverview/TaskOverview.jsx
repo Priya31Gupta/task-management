a60e7aacd033b60ea7ad37998ad651cb9969b631
Priya Gupta <priyakumarigupta790@gmail.com>
2024-11-10 20:54:06 +0530

    updated task overview

--- src/Components/taskOverview/TaskOverview.jsx
+++ src/Components/taskOverview/TaskOverview.jsx
@@ -1,24 +1,47 @@
 import './TaskOverview.scss';
 import useTaskStore from '../../store/useTaskStore';
+import { useState,useEffect } from 'react';
 
 const TaskList = () => {
     const list = useTaskStore((state) => state.list);
     const increasePopulation = useTaskStore((state) => state.increasePopulation);
+    const updateBears = useTaskStore((store) => store.updateBears)
+    const [task, setTask] = useState('');
+    const handleAddTask = (event) => {
+        if (task.trim()) { // Ensure the task is not empty
+            event.preventDefault();
+            increasePopulation({
+                name: task,
+                id: Math.random() * 10,
+                isComplete: false
+            });
+            setTask(''); // Clear the input
+        } else {
+            console.log('Task is empty, not adding.');
+        }
+    };
+
+    const handleTaskComplete = (task) => {
+console.log('✌️task --->', task);
+        updateBears({...task, isComplete:!task.isComplete });
+    };
+    
     return <div className="task-overview">
 
         <div className='flex flex-row gap-4 justify-center flex-wrap'>
-            <input type="search" className="p-1.5 rounded-sm shadow-sm h-10 w-96 text-gray-500" />
-            <button type='button' className='size-10 bg-white text-gray-500 rounded-sm' onClick={()=>{increasePopulation({
-                name:'hi',
-                id: 6,
-                isComplete: false
-            })}}>+</button>
+            <input type="text" onChange={(e)=> setTask(e.target.value)}  className="p-1.5 rounded-sm shadow-sm h-10 w-96 text-gray-500" />
+            <button 
+                type='button' 
+                className='size-10 bg-white text-gray-500 rounded-sm' 
+                onClick={handleAddTask}>
+                +
+            </button>
         </div>
         {
             list.length > 0 && list.map((task)=> {
                 return <div className='flex flex-row gap-4 justify-center flex-wrap my-1.5' key={task.id}>
-                    <div className='p-1.5 bg-white rounded-sm shadow-sm h-full w-96 text-gray-500'>{task.name} </div>
-                    <button type='button' className='bg-white text-gray-500 rounded-sm px-1 py-1'>{task.isCompleted ? 'Closed' : 'Open'}</button>
+                    <div className={`p-1.5 bg-white rounded-sm shadow-sm h-full w-96 text-gray-500 ${task.isCompleted ? 'line-through':''}`}>{task.name} </div>
+                    <button type='button' onClick={() =>handleTaskComplete(task)} className='bg-white text-gray-500 rounded-sm px-1 py-1'>{task.isCompleted ? 'Closed' : 'Open'}</button>
                 </div>
             })
         }
--- src/store/useTaskStore.js
+++ src/store/useTaskStore.js
@@ -1,37 +1,15 @@
 import { create } from 'zustand'
 
 const useTaskStore = create((set) => ({
-  list:  [
-    {
-        name:'hello-0',
-        id:0,
-        isCompleted: false,
-    },
-    {
-        name:'hello-1',
-        id:1,
-        isCompleted: false,
-    },
-    {
-        name:'hello-2',
-        id:2,
-        isCompleted: false,
-    },
-    {
-        name:'hello-3',
-        id:3,
-        isCompleted: false,
-    },
-    {
-        name:'hello-4',
-        id:4,
-        isCompleted: false,
-    },
-
-],
+  list:  [],
   increasePopulation: (newData) => set((state) => ({ list: [...state.list,newData] })),
   removeAllTasks: () => set({ list: [] }),
-  updateBears: (newBears) => set({ bears: newBears }),
+  updateBears: (task) => set((state) => ({
+    ...state,
+    list: state.list.map((item) => 
+      item.id === task.id? {...item, isComplete:!item.isComplete} : item
+    )
+  })),
 }))
 
 export default useTaskStore;
